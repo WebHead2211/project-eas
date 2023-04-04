02a46941b62376dec53efcb15fe7554dfc987c4f
Aayush Bakre <29601578+WebHead2211@users.noreply.github.com>
2023-04-05 04:31:13 +0530

    Add 2 new buttons and use random colors

--- README.md
+++ README.md
@@ -1,10 +1,21 @@
 # ETCH-A-SKETCH
 
-Use function 'gridSize(size)' to generate a grid consisting of square divs.
-gridSize(10) gives a grid of 10x10.
-Total size of the grid always remains 900px*900px.
+The page loads with a 16*16 grid.
 
-# Button
+When the mouse first moves over the grid, the squares
+take a random color (minimum possible value kept at
+rgb(200,200,200) to keep it a lighter shade)
 
-Use the button to set a custom grid size. Gris still remains 900px but the divs
-size changes to make the new grid.
+If the mouse is moved over a square again, the color gets
+darker until eventually it becomes black.
+
+# Grid Size Button
+
+Use the button to set a custom grid size.
+Total grid size always remains 900px*900px.
+Size of grid item changes to create a new grid.
+Old grid gets replaced by the new grid.
+
+# Random Color Button
+
+Use the button to reset the canvas and randomize the color.
--- index.js
+++ index.js
@@ -1,30 +1,75 @@
+//INITIAL VARIABLES
 const mainDiv = document.querySelector('#main-container');
 let mainDivWidth = mainDiv.clientWidth;
 let mainDivHeight = mainDiv.clientHeight;
+let color = randomColor();
+const safeColor = color;
+const sizeButton = document.querySelector('#size-button');
+const colorButton = document.querySelector('#color-button');
 
+
+//INITIAL GRID SETUP
 for(let i = 0; i < 256; i++){
     const div = document.createElement('div');
     div.classList.add('div-class');
     mainDiv.appendChild(div);
-    div.setAttribute('style', `width:${mainDivWidth/16}px; height:${mainDivHeight/16}px`); 
+    div.setAttribute('style', `width:${mainDivWidth/16}px; height:${mainDivHeight/16}px`);
+    div.style.backgroundColor = 'white'; 
 } 
+
+
+//EVENT LISTENERS FOR MAIN DIV CONTAINER AND THE BUTTONS
 mainDiv.addEventListener('mouseover', gridFunc);
+sizeButton.addEventListener('click', setSize);
+colorButton.addEventListener('click', newColor);
 
-const button = document.querySelector('button');
-button.addEventListener('click', setSize);
 
-function hover(){
-    this.style.backgroundColor = 'black';
+//FUNCTIONS TO ADD COLOR ON FIRST PASS AND SUBSEQUENT PASSES
+function firstPass(){
+    // let color = randomColor();
+    let red = color[0];
+    let green = color[1];
+    let blue = color[2];
+    this.style.backgroundColor = `rgb(${color})`;
 }
 
+function morePass(){
+    const thisColor = colorToArray(this.style.backgroundColor);
+    let toColor = [];
+    toColor[0] = darken(thisColor[0]);
+    toColor[1] = darken(thisColor[1]);
+    toColor[2] = darken(thisColor[2]);
+    if(toColor[0]<1){
+        toColor[0] = 0;
+    }
+    if(toColor[1]<1){
+        toColor[1] = 0;
+    }
+    if(toColor[2]<1){
+        toColor[2] = 0;
+    }
+    this.style.backgroundColor = `rgb(${toColor[0]}, ${toColor[1]}, ${toColor[2]})`;    
+}
+
+
+//FUNCTION TO ADD EVENT LISTENER TO GRID ITEMS
 function gridFunc(){
     const grid = document.querySelectorAll('.div-class');
-    grid.forEach(div => div.addEventListener('mouseover', hover));
+    grid.forEach(div => {
+        if(div.style.backgroundColor === 'white'){
+            div.addEventListener('mouseover', firstPass);
+        } else {
+            div.removeEventListener('mouseover', firstPass);
+            div.addEventListener('mouseover', morePass);
+        }
+    });
 }
 
+
+//FUNCTION TO SET CUSTOM SIZE
 function setSize(){
-    let size = window.prompt("Select a grid size (from 0 to 100)");
-    if(size>=0 && size<=100){
+    let size = window.prompt("Select a grid size (from 1 to 100)");
+    if(size>0 && size<=100){
         gridSize(size);
     } else {
         window.alert("Please choose a valid value!");
@@ -42,7 +87,37 @@ function gridSize(size){
         const div = document.createElement('div');
         div.classList.add('div-class');
         mainDiv.appendChild(div);
-        div.setAttribute('style', `width:${boxSize}px; height:${boxSize}px`); 
+        div.setAttribute('style', `width:${boxSize}px; height:${boxSize}px`);
+        div.style.backgroundColor = 'white'; 
     } 
 }
 
+
+//FUNCTION TO SET RANDOM COLOR
+function newColor(){
+    color = randomColor();
+    const divList = document.querySelectorAll('.div-class');
+    divList.forEach(div => div.style.backgroundColor = 'white');
+}
+
+function randomColor(){
+    let red = Math.floor((Math.random() * 55)+200);
+    let green = Math.floor((Math.random() * 55)+200);
+    let blue = Math.floor((Math.random() * 55)+200);
+    let color = [red, green, blue];
+    return color;
+}
+
+
+//SMALL FUNCTIONS
+function colorToArray(rgbValue){
+    let output = rgbValue.replace('rgb(', '');
+    output = output.replace(')', '');
+    output = output.replace(/\s/g,'');
+    output = output.split(',');
+    return output;
+}
+
+function darken(colorInput){
+    return colorInput - 25;
+}
