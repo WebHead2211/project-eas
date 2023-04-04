edc1ca335468d001245d221a37f9e1a80aa6c79b
Aayush Bakre <29601578+WebHead2211@users.noreply.github.com>
2023-04-05 02:13:05 +0530

    add set size button to make custom grid

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 Use function 'gridSize(size)' to generate a grid consisting of square divs.
 gridSize(10) gives a grid of 10x10.
-Total size of the grid always remains 960px*960px.
+Total size of the grid always remains 900px*900px.
+
+# Button
+
+Use the button to set a custom grid size. Gris still remains 900px but the divs
+size changes to make the new grid.
--- index.js
+++ index.js
@@ -1,7 +1,39 @@
+const mainDiv = document.querySelector('#main-container');
+let mainDivWidth = mainDiv.clientWidth;
+let mainDivHeight = mainDiv.clientHeight;
+
+for(let i = 0; i < 256; i++){
+    const div = document.createElement('div');
+    div.classList.add('div-class');
+    mainDiv.appendChild(div);
+    div.setAttribute('style', `width:${mainDivWidth/16}px; height:${mainDivHeight/16}px`); 
+} 
+mainDiv.addEventListener('mouseover', gridFunc);
+
+const button = document.querySelector('button');
+button.addEventListener('click', setSize);
+
+function hover(){
+    this.style.backgroundColor = 'black';
+}
+
+function gridFunc(){
+    const grid = document.querySelectorAll('.div-class');
+    grid.forEach(div => div.addEventListener('mouseover', hover));
+}
+
+function setSize(){
+    let size = window.prompt("Select a grid size (from 0 to 100)");
+    if(size>=0 && size<=100){
+        gridSize(size);
+    } else {
+        window.alert("Please choose a valid value!");
+    }   
+}
+
 function gridSize(size){
     let totalNumber = size*size;
-    let boxSize = 960/size;
-    const mainDiv = document.querySelector('#main-container');
+    let boxSize = mainDivHeight/size;
     if(document.querySelector('.div-class')) {
         const divList = document.querySelectorAll('.div-class');
         divList.forEach(div => mainDiv.removeChild(div));
@@ -13,3 +45,4 @@ function gridSize(size){
         div.setAttribute('style', `width:${boxSize}px; height:${boxSize}px`); 
     } 
 }
+
